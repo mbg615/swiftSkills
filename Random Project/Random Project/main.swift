fac1ce1e88e7f0a3cf070b5709ee4c9d267e8c85
mgb615 <m4dpup@gmail.com>
2020-09-09 16:46:32 -0500

    added Xcode project

---
+++ Random Project/Random Project/main.swift
@@ -0,0 +1,51 @@
+//
+//  Dice Project.swift
+//
+//
+//  Created by Maddox on 9/1/20.
+//
+
+import Foundation
+import AppKit
+
+var run:Int
+
+print("Roll a Die?", terminator: " ")
+var start = readLine()
+if start == "yes" {
+    run = 0
+    while run == 0 {
+        let die = Int.random(in: 1...6)
+        
+        let red:Int = Int.random(in: 0...256)
+        let green:Int = Int.random(in: 0...256)
+        let blue:Int = Int.random(in: 0...256)
+        
+        func RGB_to_NSColor(red: Int, green:Int, blue:Int) -> NSColor {
+            return NSColor(
+            red: CGFloat((red & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((green & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(blue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+            )
+        }
+        
+        let textColor: NSColor = RGB_to_NSColor(red: red, green: green, blue: blue)
+        
+        print("You rolled a \(die) with NSColor \(textColor)")
+        print("Would you like to roll again?", terminator: " ")
+        
+        let cont = readLine()
+        if cont != "no"{
+            run = 0
+        }
+        else {
+            run = 1
+            print("Goodbye!")
+        }
+        
+    }
+}
+else {
+    print("Goodbye!")
+}
